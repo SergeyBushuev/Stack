7e6c7989db131994d25a53112ea6efa073ecb17d
Varvar <Sergeybushuev14@gmail.com>
2016-09-10 16:05:08 +0300

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef Stack_H
 #define Stack_H
 
@@ -14,6 +15,7 @@ public:
 	size_t count() const;
 	void push(T const &);
 	T pop();
+	void print(int);
 private:
 	T * array_;
 	size_t array_size_;
@@ -21,7 +23,7 @@ private:
 };
 template <typename T>
 stack<T>::stack() :
-	count_(0){
+	count_(0) {
 }
 template <typename T>
 stack<T>::~stack()
@@ -31,20 +33,46 @@ stack<T>::~stack()
 template <typename T>
 void stack<T>::push(const T &value)
 {
-	assert(count_ < array_size_); 
+	if (count_ == 0) {
+		array_ = new T[count_+1];
+		array_[count_] = value;
+		++count_;
+		array_size_ = count_;
 
-	array_[count_++] = value;
+	}
+	else if (count_ == array_size_) {
+		array_size_++;
+		T * nstack = new T[array_size_];
+		for (size_t i = 0; i < array_size_; ++i) {
+			nstack[i] = array_[i];
+		}
+		delete[] array_;
+		array_ = nstack;
+		array_[count_] = value;
+		++count_;
+	}
+	else if (count_ < array_size_) {
+		array_[count_] = value;
+		count_++;
+	}
+
 }
 template <typename T>
 T stack<T>::pop()
 {
 	assert(count_ > 0);
 
-	array_[--count_]; 
+	return array_[--count_];
+
 }
 template <typename T>
 size_t stack<T>::count() const
 {
 	return count_;
+}
+template <typename T>
+void stack<T>::print(int c)
+{
+		cout << array_[c] << endl;
 }
 #endif // Stack_H
