cf8c159bc8d9e286427c92cdd7c7ca101cc8d93d
Varvar <Sergeybushuev14@gmail.com>
2016-09-18 15:25:30 +0300

    Create init.cpp

---
+++ tests/source/init.cpp
@@ -0,0 +1,24 @@
+#include <Stack.h>
+#include <catch.hpp>
+#include <iostream>
+using namespace std;
+
+SCENARIO("count", "[count]"){
+  stack<int> s;
+  s.push(1);
+  REQUIRE(s.count()==1);
+}
+
+SCENARIO("push", "[push]"){
+  stack<int> s;
+  s.push(1);
+  REQUIRE(s.count()==1);
+  REQUIRE(s.pop()==1);
+}
+
+SCENARIO("pop", "[pop]"){
+  stack<int> s;
+  s.push(1);
+  REQUIRE(s.count()==1);
+  REQUIRE(s.pop()==1);
+}
