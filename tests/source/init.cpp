191024920d50fc1d5777778b1367f1f171c22121
Varvar <Sergeybushuev14@gmail.com>
2016-09-19 18:00:12 +0400

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -24,14 +24,14 @@ SCENARIO("pop", "[pop]"){
 }
 SCENARIO("copy", "[copy]"){
   stack<int> stack, stack2;
-  stack1.push(1);
+  stack.push(1);
   stack2=stack;
   REQUIRE(stack2.count()==1);
   REQUIRE(stack2.pop()==1);
 }
 SCENARIO("copy2", "[copy2]"){
   stack<int> stack1;
-  stack.push(1);
+  stack1.push(1);
   stack<int> stack2= stack1;
   REQUIRE(stack2.count()==1);
   REQUIRE(stack2.pop()==1);
