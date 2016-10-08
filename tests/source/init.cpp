54861235f9eecdacaeba6f37e842c72877961f74
Varvar <Sergeybushuev14@gmail.com>
2016-10-08 13:05:45 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -8,3 +8,30 @@ SCENARIO("Stack: init", "[init]") {
 	REQUIRE(sizeof(a) != 0);
 } 
 
+SCENARIO("push", "[push]"){ 
+  stack<size_t> stack;
+  stack.push(1);
+  REQUIRE(stack.count()==1);
+  REQUIRE(stack.top()==1);
+}
+
+SCENARIO("pop", "[pop]"){
+  stack<size_t> stack;
+  stack.push(1);
+  REQUIRE(stack.count()==1);
+  REQUIRE(stack.top()==1);
+}
+SCENARIO("copy", "[copy]"){
+  stack<size_t> stack, stack2;
+  stack.push(1);
+  stack2=stack;
+  REQUIRE(stack2.count()==1);
+  REQUIRE(stack2.top()==1);
+}
+SCENARIO("copy2", "[copy2]"){
+  stack<size_t> stack1;
+  stack1.push(1);
+  stack<size_t> stack2= stack1;
+  REQUIRE(stack2.count()==1);
+  REQUIRE(stack2.top()==1);
+}
