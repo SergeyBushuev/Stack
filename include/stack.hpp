ea15449cad40bfe0f8cc5938091a8549772c32f0
Varvar <Sergeybushuev14@gmail.com>
2016-11-13 23:42:30 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -139,7 +139,7 @@ auto allocator<T>::resize()->void
 {
 	allocator<T> all(size_ * 2 + (size_ == 0));
 	for (size_t i = 0; i < size_; ++i) 
-		if (test(i)){
+		if (test(i)==1){
 		all.construct(all.get() + i, ptr_[i]);
 		}
 	this->swap(all);
@@ -163,7 +163,7 @@ ptr_(static_cast<T *>(other.size_ == 0 ? nullptr : operator new(other.size_ * si
 	size_(other.size_), 
 	map_(std::make_unique<bitset>(other.size_)) {
 	for (size_t i = 0; i < size_; ++i) {
-		if (test(i)){
+		if (test(i)==1){
 		construct(ptr_ + i, other.ptr_[i]);
 		}
 	}
