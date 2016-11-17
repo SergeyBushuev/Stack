600a0107a4cc4b977f398a625c16146a26b185d9
Varvar <Sergeybushuev14@gmail.com>
2016-11-17 13:26:26 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -162,7 +162,7 @@ allocator<T>::allocator(allocator const& other) :
 ptr_(static_cast<T *>(other.size_ == 0 ? nullptr : operator new(other.size_ * sizeof(T)))), 
 	size_(other.size_), 
 	map_(std::make_unique<bitset>(other.size_)) {
-	for (size_t i = 0; i < other.map_.count(); ++i) {
+	for (size_t i = 0; i < other.size_; ++i) {
 		if (other.map_->test(i)){
 		this->construct(ptr_ + i, other.ptr_[i]);
 		}
